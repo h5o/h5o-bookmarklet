f21491101bc20e33c0f52bc7a7522f5ff6fd81d7
Dominykas Blyžė <hello@dominykas.com>
2015-04-12 20:40:27 +0300

    autosize the iframe - closes #3

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,10 +4,22 @@ var HTML5Outline = require("h5o"),
 	Iframeish = require("./Iframeish");
 
 var setAttribute = function (els, attr, val) {
-	for (var i = 0; i < els.length; i++) els[i].setAttribute(attr, val);
+	for (var i = 0; i < els.length; i++) {
+		els[i].setAttribute(attr, val);
+	}
 };
 
-var IFRAME_STYLE = 'position:fixed;top:10px;right:10px;border:2px solid #000;background:rgba(255,255,255,.9);z-index:999999;width:400px;';
+var IFRAME_BORDER_SIZE = 2;
+var IFRAME_STYLE = '' +
+	'position:fixed;' +
+	'top:10px;' +
+	'right:10px;' +
+	'border:' + IFRAME_BORDER_SIZE + 'px solid #000;' +
+	'background:rgba(255,255,255,.9);' +
+	'z-index:999999;' +
+	'max-width:90%;' +
+	'max-width:calc(100% - 20px);' +
+	'min-height:50px;';
 
 var outlineHTML = HTML5Outline(document.body).asHTML(true);
 
@@ -31,8 +43,8 @@ Iframeish(function (err, iframeish) {
 	setAttribute([iframeish.iframe], "style", IFRAME_STYLE);
 	iframeish.document.body.appendChild(containerDiv);
 
-	var size = containerDiv.getBoundingClientRect();
-	iframeish.iframe.style.height = (size.height > 400 ? 400 : size.height) + "px";
+	iframeish.iframe.style.width = containerDiv.scrollWidth + "px";
+	iframeish.iframe.style.height = (containerDiv.scrollHeight + IFRAME_BORDER_SIZE) + "px";
 
 	lnk.addEventListener("click", function () {
 		document.body.removeChild(iframeish.iframe);
